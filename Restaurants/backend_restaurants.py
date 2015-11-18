2d4bdd98da813b9938256b762a1feccc0e1113db
ludgo <ludgogit@gmail.com>
2015-11-18 14:17:38 +0100

    code

---
+++ Restaurants/backend_restaurants.py
@@ -0,0 +1,153 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+app = Flask(__name__)
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+@app.route('/')
+@app.route('/restaurant/')
+@app.route('/restaurants/')
+def showRestaurants():
+    restaurants = session.query(Restaurant).all()
+    return render_template('showRestaurants.html', restaurants=restaurants)
+
+@app.route('/restaurant/new/', methods=['GET', 'POST'])
+def newRestaurant():
+    if request.method == 'POST':
+        newRestaurant = Restaurant(
+            name=request.form['newName'],
+            description=request.form['newDescription'],
+            city=request.form['newCity'],
+            state=request.form['newState'],
+            )
+        session.add(newRestaurant)
+        session.commit()
+        flash("restaurant created")
+        return redirect(url_for('showRestaurants'))
+    else:
+        return render_template('newrestaurant.html')
+
+@app.route('/restaurant/<int:restaurant_id>/edit/', methods=['GET', 'POST'])
+def editRestaurant(restaurant_id):
+    editedRestaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    if request.method == 'POST':
+        editedRestaurant.name = request.form['editedName']
+        editedRestaurant.description = request.form['editedDescription']
+        editedRestaurant.city = request.form['editedCity']
+        editedRestaurant.state = request.form['editedState']
+        session.add(editedRestaurant)
+        session.commit()
+        flash("restaurant edited")
+        return redirect(url_for('showRestaurants'))
+    else:
+        return render_template('editrestaurant.html', editedRestaurant=editedRestaurant)
+
+@app.route('/restaurant/<int:restaurant_id>/delete/', methods=['GET', 'POST'])
+def deleteRestaurant(restaurant_id):
+    deletedRestaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    if request.method == 'POST':
+        session.delete(deletedRestaurant)
+        session.commit()
+        flash("restaurant deleted")
+        return redirect(url_for('showRestaurants'))
+    else:
+        return render_template('deleterestaurant.html', deletedRestaurant=deletedRestaurant)
+
+@app.route('/restaurant/<int:restaurant_id>/')
+@app.route('/restaurant/<int:restaurant_id>/menu/')
+def showMenu(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+    return render_template('showmenu.html', restaurant=restaurant, items=items)
+
+@app.route('/restaurant/<int:restaurant_id>/menu/new/', methods=['GET', 'POST'])
+def newMenuItem(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    if request.method == 'POST':
+        newItem = MenuItem(
+            name=request.form['newName'],
+            restaurant_id=restaurant_id,
+            description = request.form['newDescription'],
+            price = request.form['newPrice'],
+            course = request.form['newCourse']
+            )
+        session.add(newItem)
+        session.commit()
+        flash("menu item created")
+        return redirect(url_for('showMenu', restaurant_id=restaurant_id))
+    else:
+        courses = ['Appetizer', 'Beverage', 'Dessert', 'Entree']
+        return render_template('newmenuitem.html', restaurant=restaurant, courses=courses)
+
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit/', methods=['GET', 'POST'])
+def editMenuItem(restaurant_id, menu_id):
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    editedItem = session.query(MenuItem).filter_by(id = menu_id).one()
+    if request.method == 'POST':
+        editedItem.name = request.form['editedName']
+        editedItem.description = request.form['editedDescription']
+        editedItem.price = request.form['editedPrice']
+        editedItem.course = request.form['editedCourse']
+        session.add(editedItem)
+        session.commit()
+        flash("menu item edited")
+        return redirect(url_for('showMenu', restaurant_id=restaurant_id))
+    else:
+        courses = ['Appetizer', 'Beverage', 'Dessert', 'Entree']
+        return render_template('editmenuitem.html', restaurant=restaurant, editedItem=editedItem, courses=courses)
+
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete/', methods=['GET', 'POST'])
+def deleteMenuItem(restaurant_id, menu_id):
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    deletedItem = session.query(MenuItem).filter_by(id = menu_id).one()
+    if request.method == 'POST':
+        session.delete(deletedItem)
+        session.commit()
+        return redirect(url_for('showMenu', restaurant_id=restaurant_id))
+        flash("menu item deleted")
+    else:
+        return render_template('deletemenuitem.html', restaurant=restaurant, deletedItem=deletedItem)
+
+# JSON
+
+@app.route('/json/')
+@app.route('/restaurant/json/')
+@app.route('/restaurants/json/')
+@app.route('/JSON/')
+@app.route('/restaurant/JSON/')
+@app.route('/restaurants/JSON/')
+def restaurantsJSON():
+    restaurants = session.query(Restaurant).all()
+    return jsonify(Restaurants=[r.serialize for r in restaurants])
+
+@app.route('/restaurant/<int:restaurant_id>/json/')
+@app.route('/restaurant/<int:restaurant_id>/menu/json/')
+@app.route('/restaurant/<int:restaurant_id>/JSON/')
+@app.route('/restaurant/<int:restaurant_id>/menu/JSON/')
+def restaurantMenuJSON(restaurant_id):
+    items = session.query(MenuItem).filter_by(
+        restaurant_id=restaurant_id).all()
+    return jsonify(MenuItems=[i.serialize for i in items])
+
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/json/')
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/json/')
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/JSON/')
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/JSON/')
+def menuItemJSON(restaurant_id, menu_id):
+    item = session.query(MenuItem).filter_by(
+        id=menu_id).one()
+    return jsonify(MenuItem=item.serialize)
+ 
+# end JSON
+
+if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
---
+++ Restaurants/database_setup.py
@@ -0,0 +1,56 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+ 
+Base = declarative_base()
+
+#Class
+class Restaurant(Base):
+	#Table
+    __tablename__ = 'restaurant'
+   
+    #Mappers
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    description = Column(String(250))
+    city = Column(String(50))
+    state = Column(String(50))
+
+    @property
+    def serialize(self):
+
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'city': self.city,
+            'state': self.state,
+        }
+ 
+class MenuItem(Base):
+    __tablename__ = 'menu_item'
+
+    name = Column(String(50), nullable = False)
+    id = Column(Integer, primary_key = True)
+    description = Column(String(250))
+    price = Column(String(8))
+    course = Column(String(50))
+    restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
+
+    @property
+    def serialize(self):
+
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'price': self.price,
+            'course': self.course,
+        }
+ 
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.create_all(engine)
